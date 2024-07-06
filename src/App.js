7f2375ce51c94ad81a30ec2c816335caa69581ee
Jai-jayapalan <jaijayathilak@gmail.com>
2024-07-06 15:34:45 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import ProductList from './components/ProductList';
+import Register from './components/Register';
+import Login from './components/Login';
+import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
   return (
-    <div>
-      <ProductList />
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<ProtectedRoute />}>
+            <Route path="/products" element={<ProductList />} />
+          </Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
